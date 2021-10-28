fb2ca5a2db30c99099c43cd22ff30912a0a87d6a
BenjaminCF <91268342+BenjaminCF@users.noreply.github.com>
2021-10-28 18:45:44 +0200

    Add files via upload

---
+++ Mario/fichiers.h
@@ -0,0 +1,9 @@
+//--------Rattrapage BRPI BENJAMIN CAPRIOLI-FATON--------
+#ifndef DEF_FICHIERS
+#define DEF_FICHIERS
+
+    int chargerNiveau(int niveau[][NB_BLOCS_HAUTEUR]);
+    int sauvegarderNiveau(int niveau[][NB_BLOCS_HAUTEUR]);
+
+#endif
+
