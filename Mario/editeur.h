0e8d8127308e7f95bc255858199bf45e819c4310
BenjaminCF <91268342+BenjaminCF@users.noreply.github.com>
2021-10-28 18:43:42 +0200

    Delete Mario directory

--- Mario/editeur.h
+++
@@ -1,6 +0,0 @@
-#ifndef DEF_EDITEUR
-#define DEF_EDITEUR
-
-    void editeur(SDL_Surface* ecran);
-
-#endif
--- Mario/jeu.h
+++
@@ -1,9 +0,0 @@
-#ifndef DEF_JEU
-#define DEF_JEU
-
-    void jouer(SDL_Surface* ecran);
-    void deplacerJoueur(int carte[][NB_BLOCS_HAUTEUR], SDL_Rect *pos, int direction);
-    void deplacerCaisse(int *premiereCase, int *secondeCase);
-
-
-#endif
