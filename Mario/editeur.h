82813230df312de575554f5066336f8b087a963f
BenjaminCF <91268342+BenjaminCF@users.noreply.github.com>
2021-10-28 18:17:16 +0200

    Update editeur.h

--- Mario/editeur.h
+++ Mario/editeur.h
@@ -1,12 +1,3 @@
-/*
-editeur.h
----------
-
-Par mateo21, pour Le Site du Zér0 (www.siteduzero.com)
-
-Rôle : prototypes des fonctions de gestion de l'éditeur de niveaux.
-*/
-
 #ifndef DEF_EDITEUR
 #define DEF_EDITEUR
 
