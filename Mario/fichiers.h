83c7e70d8ae36b7f1c5884b0c7519eb561a0aaaa
BenjaminCF <91268342+BenjaminCF@users.noreply.github.com>
2021-10-28 18:18:36 +0200

    Update fichiers.h

--- Mario/fichiers.h
+++ Mario/fichiers.h
@@ -1,12 +1,3 @@
-/*
-fichiers.h
-----------
-
-Par mateo21, pour Le Site du Zér0 (www.siteduzero.com)
-
-Rôle : prototypes des fonctions de lecture / écriture de fichiers de niveau.
-*/
-
 #ifndef DEF_FICHIERS
 #define DEF_FICHIERS
 
